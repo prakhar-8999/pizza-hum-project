d0876c5236f9f0b3b7c44bd29e48caf35b89619d
root <prakharporwal12a@gmail.com>
2023-09-15 09:28:41 +0000

    fix: serverless function

---
+++ components/Success.vue
@@ -0,0 +1,25 @@
+<script setup>
+const getPaymentDetails = async () => {
+  const {data, status} = await useAPIFetch("saveOrder/", {
+    method: "POST",
+    body: {session: localStorage.getItem("paymentSession")},
+  });
+  if (data.value && status.value === "success") {
+    console.log(JSON.parse(JSON.stringify(data.value)));
+  }
+};
+getPaymentDetails();
+</script>
+
+<template>
+  <div
+    v-motion="{
+      initial: {opacity: 0, x: -200},
+      enter: {opacity: 1, x: 0},
+      transition: {duration: 0.5, ease: 'easeInOut'},
+    }"
+    class="rounded-2xl bg-stone-100 px-10 px-4 py-5 py-6 shadow-lg"
+  >
+    <h1 class="text-center">Payment Successfull</h1>
+  </div>
+</template>
