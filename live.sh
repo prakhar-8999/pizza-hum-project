e693c176eefd732aadf788ff4bc15160112d96ec
root <prakharporwal12a@gmail.com>
2023-09-17 19:27:29 +0000

    feat: updating script for making code in production

--- live.sh
+++ live.sh
@@ -7,11 +7,12 @@ rm -rf .output
 echo "✨ Uploading"
 scp -r ~/Documents/free-lancing/pizza-hum-project/pizza-hum/* root@85.31.238.178:/root/main/pizza-hum/pizza_hum_frontend
 ssh root@85.31.238.178 "
-cd /root/main/pizza-hum-project/
-docker compose down
+cd /root/main/pizza-hum/pizza_hum_frontend
 rm -rf .output
 rm -rf .nuxt
 rm -rf node_modules
+cd ..
+docker compose down
 docker-compose up -d --build
 exit "
 echo "🔥🔥 Live"
