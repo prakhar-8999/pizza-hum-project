7b0964ef021461db82f5f0a0fc141c9624a64f36
root <prakharporwal12a@gmail.com>
2023-09-19 16:31:02 +0000

    fix: removing dollar sign

--- components/Orders.vue
+++ components/Orders.vue
@@ -222,7 +222,7 @@ const generateSessionID = async (event) => {
       @click="submitPayment"
       class="w-full inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-3 md:px-6 md:py-4"
     >
-      Pay Now $
+      Pay Now
       {{
         new Intl.NumberFormat("en", {
           style: "currency",
